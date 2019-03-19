b30ecc8974dabcfd067a4245645978847d867613
raidenx <obbeyyusuf.p@gmail.com>
2019-03-19 11:42:50 +0700

    Updadate App

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import {BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import cookies from 'universal-cookie'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
-import {keepLogin} from '../actions'
+import { keepLogin } from '../actions'
 
 import Home from './Home'
 import Header from './Header'
@@ -16,33 +16,30 @@ import DetailProduct from './DetailProduct';
 const cookie = new cookies()
 
 class App extends Component {
-    // life cycle hook / method
-    
-    componentDidMount(){
-        // akan di jalankan sekali ketika pertama kali component di render
 
-        // mengambil value yang disimpan pada file cookie masihLogin
+
+    componentDidMount() {
+
         var userCookie = cookie.get('masihLogin')
-        // jika didapatkan username di file cookie, akan memanggil function keepLogin
-        if(userCookie !== undefined){
-            // function keepLogin akan me-loginkan ulang username yg tersimpan pada file cookie
+
+        if (userCookie !== undefined) {
+
             this.props.keepLogin(userCookie)
         }
 
     }
 
-    render () {
+    render() {
         return (
             <BrowserRouter>
                 <div>
-                    {/* link.contains("/login") */}
-                    {/* link === "/login" */}
-                    <Header/>
-                    <Route path="/" exact component={Home}/>
+
+                    <Header />
+                    <Route path="/" exact component={Home} />
                     <Route path="/login" component={Login} />
                     <Route path="/register" component={Register} />
-                    <Route path="/manageproduct" component={ManageProduct}/>
-                    <Route path="/detailproduct/:asdfg" component={DetailProduct}/>
+                    <Route path="/manageproduct" component={ManageProduct} />
+                    <Route path="/detailproduct/:asdfg" component={DetailProduct} />
                 </div>
             </BrowserRouter>
         )
@@ -50,4 +47,4 @@ class App extends Component {
 
 }
 
-export default connect(null, {keepLogin})(App)
+export default connect(null, { keepLogin })(App)
