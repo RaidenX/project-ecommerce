759b676bf09289b466fcafb662c41e440986d302
raidenx <obbeyyusuf.p@gmail.com>
2019-03-20 15:33:38 +0700

    Add More Update

--- src/components/ProductCart.js
+++ src/components/ProductCart.js
@@ -1,180 +1,219 @@
-import React, { Component } from 'react'
-import { connect }  from 'react-redux'
-import axios from 'axios'
-import Cookies from 'universal-cookie'
-import {Redirect} from 'react-router-dom'
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import axios from "axios";
+import Cookies from "universal-cookie";
+import { Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import Bill from './Bill'
+import Bill from "./Bill";
 
-const cookie = new Cookies()
+const cookie = new Cookies();
 
 class ProductCart extends Component {
   // format IDR
   constructor(props) {
     super(props);
-    this.formatterIDR = new Intl.NumberFormat('id', {
-        style: 'currency',
-        currency: 'IDR',
-        minimumFractionDigits: 0
-      })
-      
-}
+    this.formatterIDR = new Intl.NumberFormat("id", {
+      style: "currency",
+      currency: "IDR",
+      minimumFractionDigits: 0
+    });
+  }
 
   // to prevent warning memory leaks on your damn browser!!
   _isMounted = false;
 
-  state ={
-      cart:[],
-      selectedIdx:0
+  state = {
+    cart: [],
+    selectedIdx: 0
+  };
+
+  componentDidMount() {
+    this.getCart();
   }
-  
-  componentDidMount(){
-        
-        this.getCart()
-      
-  }
-  
+
   // referensi
   // https://www.robinwieruch.de/react-warning-cant-call-setstate-on-an-unmounted-component/
-  componentWillUnmount(){
-    this._isMounted = false
-    
+  componentWillUnmount() {
+    this._isMounted = false;
   }
-  
-  getCart =()=>{
-    this._isMounted =true
-      axios.get('http://localhost:1806/carts',{
-        params:{
-           username: cookie.get("masihLogin")
-          
+
+  getCart = () => {
+    this._isMounted = true;
+    axios
+      .get("http://localhost:1806/carts", {
+        params: {
+          username: cookie.get("masihLogin")
         }
       })
-      .then(res=>{
-        if(this._isMounted){
-          this.setState({cart:res.data, selectedIdx:0})
+      .then(res => {
+        if (this._isMounted) {
+          this.setState({ cart: res.data, selectedIdx: 0 });
         }
-          
-      })
-  }
+      });
+  };
 
   // Remove Product from Cart
-  onCartDel = (id) => {
-    const ask = window.confirm('Are you sure to remove this items?? :(')
-    
-    if (ask === true){
-      const delId = id
-    
-      axios.delete(`http://localhost:1806/carts/${delId}`)
-  
+  onCartDel = id => {
+    const ask = window.confirm("Apakah anda yakin menghapus produk ini?? :(");
+
+    if (ask === true) {
+      const delId = id;
+
+      axios
+        .delete(`http://localhost:1806/carts/${delId}`)
+
         .then(res => {
-          this.getCart() 
-        })
+          this.getCart();
+        });
     } else {
-      alert('Thanks God you did not do it :)')
+      alert("Terima kasih, untuk tetap memilih produk kami :)");
     }
-    
-  }
+  };
 
   //Revise Product Quantity
-  onCartEdit = id =>{
-     this.setState({selectedIdx:id})
-  }
+  onCartEdit = id => {
+    this.setState({ selectedIdx: id });
+  };
 
-  onSaveCart = id =>{
-    
-    axios.get(`http://localhost:1806/carts/${id}`)
-      .then(res=>{
-          const user = res.data.username
-          const prodId = res.data.productId
-          const name = res.data.name
-          const desc = res.data.desc
-          const price = res.data.price
-          const src = res.data.src
-          const id = res.data.id
+  onSaveCart = id => {
+    axios.get(`http://localhost:1806/carts/${id}`).then(res => {
+      const user = res.data.username;
+      const prodId = res.data.productId;
+      const name = res.data.name;
+      const desc = res.data.desc;
+      const price = res.data.price;
+      const src = res.data.src;
+      const id = res.data.id;
 
-          if(user && prodId){
-            const eQty = parseInt(this.qty.value)
-            axios.put(`http://localhost:1806/carts/${id}`, {
-              productId: prodId,
-              username: user,
-              name,
-              qty: eQty,
-              desc,
-              price,
-              src,
-              id,
-            }).then(res => {
-              this.getCart()
-              this.checkout()
-            })
-          }
-          
-      })
-     
-  }
-
+      if (user && prodId) {
+        const eQty = parseInt(this.qty.value);
+        axios
+          .put(`http://localhost:1806/carts/${id}`, {
+            productId: prodId,
+            username: user,
+            name,
+            qty: eQty,
+            desc,
+            price,
+            src,
+            id
+          })
+          .then(res => {
+            this.getCart();
+            this.checkout();
+          });
+      }
+    });
+  };
 
   //checkout cart
   checkout = () => {
     if (this.state.flag) {
-      return <Bill list={this.state.cart} />
+      return <Bill list={this.state.cart} />;
     } else {
-      return (<div className="d-flex justify-content-center">
-        <button onClick={this.clickCheckOutBtn} className="btn btn-outline-primary mt-5">Check Out</button>
-      </div>)
+      return (
+        <div className="d-flex justify-content-center">
+          <button
+            onClick={this.clickCheckOutBtn}
+            className="btn btn-outline-primary mt-5"
+          >
+            Check Out
+          </button>
+        </div>
+      );
     }
-  }
+  };
 
   clickCheckOutBtn = () => {
-    this.setState({ flag: true })
-  }
+    this.setState({ flag: true });
+  };
 
-  cartList =()=>{
-    return this.state.cart.map(item=>{
-       if(item.id!== this.state.selectedIdx){
-        return(
+  cartList = () => {
+    return this.state.cart.map(item => {
+      if (item.id !== this.state.selectedIdx) {
+        return (
           <tbody>
-          <tr key={item.id}>
-            <td>{item.productId}</td>
-            <td>{item.name}</td>
-            <td>{item.desc}</td>
-            <td>{this.formatterIDR.format(item.price)}</td>
-            <td>
-              <img className="list" src={item.src} alt={item.src} />
-            </td>
-            <td>{item.qty}</td>
-            <td><button onClick={()=>{this.onCartEdit(item.id)}} className="btn btn-success">+/-</button></td>
-            
-            
-            <td><button onClick={()=>{this.onCartDel(item.id)}} className="btn btn-danger mr-2">Remove</button></td>
-          </tr>
+            <tr key={item.id}>
+              <td>{item.productId}</td>
+              <td>{item.name}</td>
+              <td>{item.desc}</td>
+              <td>{this.formatterIDR.format(item.price)}</td>
+              <td className="text-md-center">
+                <img className="list" src={item.src} alt={item.src} />
+              </td>
+              <td className="text-md-center">{item.qty}</td>
+              <td>
+                <button
+                  onClick={() => {
+                    this.onCartEdit(item.id);
+                  }}
+                  className="btn btn-success"
+                >
+                  +/-
+                </button>
+              </td>
+
+              <td>
+                <button
+                  onClick={() => {
+                    this.onCartDel(item.id);
+                  }}
+                  className="btn btn-danger mr-2"
+                >
+                  Remove
+                </button>
+              </td>
+            </tr>
           </tbody>
-        )
-       } else {
-        return(
+        );
+      } else {
+        return (
           <tr key={item.id}>
             <td>{item.productId}</td>
             <td>{item.name}</td>
             <td>{item.desc}</td>
             <td>Rp {item.price}</td>
-            <td>
+            <td className="text-md-center">
               <img className="list" src={item.src} alt={item.src} />
             </td>
-            <td><input ref={input => this.qty = input} className="form-control" type="number" defaultValue={item.qty}></input></td>
-    
-            <td><button onClick={()=>{this.onSaveCart(item.id)}} className="btn btn-success">Save</button></td>
-            <td><button onClick={()=>{this.onCartDel(item.id)}} className="btn btn-danger mr-2">Remove</button></td>
+            <td className="text-md-center">
+              <input
+                ref={input => (this.qty = input)}
+                className="form-control"
+                type="number"
+                defaultValue={item.qty}
+              />
+            </td>
+
+            <td>
+              <button
+                onClick={() => {
+                  this.onSaveCart(item.id);
+                }}
+                className="btn btn-success"
+              >
+                Save
+              </button>
+            </td>
+            <td>
+              <button
+                onClick={() => {
+                  this.onCartDel(item.id);
+                }}
+                className="btn btn-danger mr-2"
+              >
+                Remove
+              </button>
+            </td>
           </tr>
-        )
-  
-       }
-    })
-  }
+        );
+      }
+    });
+  };
   render() {
     if (this.props.username !== "") {
-
-      if (this.state.cart.length > 0) {        
+      if (this.state.cart.length > 0) {
         return (
           <div className="container">
             <h1 className="display-4 text-center">Your Cart</h1>
@@ -190,31 +229,33 @@ class ProductCart extends Component {
                   <th scope="col">ACTION</th>
                 </tr>
               </thead>
-                {this.cartList()}
+              {this.cartList()}
             </table>
             <div className="container">
               <h1 className="display-4 text-center">Total</h1>
-                {this.checkout()}
+              {this.checkout()}
             </div>
           </div>
-        )
+        );
       } else {
         return (
           <div className="container mt-5">
             <div className=" row d-flex justify-content-center">
-              <h1 className="display-4">YOU DON'T HAVE ANY ITEM ON CART PLEASE GOT TO <Link to='/'>ALL PRODUCTS</Link> </h1>
+              <h1 className="display-4">
+                YOU DON'T HAVE ANY ITEM ON CART PLEASE GO TO{" "}
+                <Link to="/">ALL PRODUCTS</Link>{" "}
+              </h1>
             </div>
           </div>
-        )
+        );
       }
     } else {
-      return (<Redirect to="/" />)
+      return <Redirect to="/" />;
     }
   }
 }
 
-const mapStateToProps = state =>{
-  return {username: state.auth.username}
-}
-export default connect (mapStateToProps)(ProductCart)
-
+const mapStateToProps = state => {
+  return { username: state.auth.username };
+};
+export default connect(mapStateToProps)(ProductCart);
